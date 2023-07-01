20d74f45136da53c597c72d30ef6a32771287a99
migecvr <101593533+migecvr@users.noreply.github.com>
2023-06-30 23:40:16 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASPModel/DTO/Medidor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorASPModel.DTO
+{
+    public class Medidor
+    {
+        private int numSerie;
+        private string tipo;
+
+        public int NumSerie { get => numSerie; set => numSerie = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+
+
+
+
+        //private string nombre;
+        //private string codigo;
+
+        //public string Nombre { get => nombre; set => nombre = value; }
+        //public string Codigo { get => codigo; set => codigo = value; }
+    }
+}
