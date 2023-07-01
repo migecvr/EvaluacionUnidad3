5fb64b1fbc445ef232ad8e79a2eebf73c33f6176
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-01 18:59:38 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASPModel/DAL/LecturasDALObjetos.cs
@@ -0,0 +1,23 @@
+﻿using MedidorASPModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorASPModel.DAL
+{
+    public class LecturasDALObjetos : ILecturasDAL
+    {
+        private static List<Lectura> lecturas = new List<Lectura>();
+        public void AgregarLectura(Lectura lectura)
+        {
+            lecturas.Add(lectura);
+        }
+
+        public List<Lectura> ObtenerLecturas()
+        {
+            return lecturas;
+        }
+    }
+}
