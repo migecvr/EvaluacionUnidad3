aec33ec468e6c5f211bd421beb54200f5ce0efe3
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-01 19:05:08 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASP/IngresarLectura.aspx.cs
@@ -0,0 +1,73 @@
+﻿using MedidorASPModel.DAL;
+using MedidorASPModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedidorASP
+{
+    public partial class IngresarLectura : System.Web.UI.Page
+    {
+        private ILecturasDAL lecturasDAL = new LecturasDALObjetos();
+        private IMedidoresDAL medidoresDAL = new MedidoresDALObjetos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                List<Medidor> medidores = medidoresDAL.ObtenerMedidores();
+                this.medidorDdl.DataSource = medidores;
+                this.medidorDdl.DataTextField = "NumSerie";
+                this.medidorDdl.DataValueField = "NumSerie";
+                this.medidorDdl.DataBind();
+            }
+                
+        }
+
+
+        protected void btnIngresarLectura_Click(object sender, EventArgs e)
+        {
+            
+
+
+            string medidorValor = this.medidorDdl.SelectedValue;
+            //obtiene el valor del Texto del DropDown
+            string medidorTexto = this.medidorDdl.SelectedItem.Text;
+
+
+
+            String fecha = this.FechaTxt.Text.Trim();
+
+            int hora = Convert.ToInt32(this.HoraTxt.Text.Trim());
+
+            int minuto = Convert.ToInt32(this.MinutoTxt.Text.Trim());
+
+            int consumo = Convert.ToInt32(this.ConsumoTxt.Text.Trim());
+
+
+
+            List<Medidor> medidores = medidoresDAL.ObtenerMedidores();
+
+            Medidor medidor = medidores.Find(m => m.NumSerie == Convert.ToInt32(this.medidorDdl.SelectedItem.Value));
+
+
+
+            Lectura lectura = new Lectura()
+            {
+                Medidor = medidor,
+                Fecha = fecha,
+                Hora = hora,
+                Minuto = minuto,
+                Consumo = consumo
+            };
+            lecturasDAL.AgregarLectura(lectura);
+
+            this.mensajeLbl.Text = "Ingesado Correctamente";
+
+            Response.Redirect("VerLecturas.aspx");
+        }
+    }
+}
---
+++ MedidorASP/MedidorASP/VerLecturas.aspx.cs
@@ -0,0 +1,78 @@
+﻿using MedidorASPModel.DAL;
+using MedidorASPModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedidorASP
+{
+    public partial class VerLecturas : System.Web.UI.Page
+    {
+        private ILecturasDAL lecturasDAL = new LecturasDALObjetos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Lectura> lecturas = lecturasDAL.ObtenerLecturas();
+            this.grillaLecturas.DataSource = lecturas;
+
+            this.grillaLecturas.DataBind();
+
+            
+            if (!IsPostBack)
+            {
+                List<int> numsSeriess = devuelveNSeries();
+                for(int i=0; i < numsSeriess.Count(); i++)
+                {
+                    this.FltLecturaDdl.Items.Add(new ListItem(numsSeriess[i].ToString(), i.ToString()));
+                }
+
+                
+            }
+
+        }
+
+        protected void CargaGrilla()
+        {
+
+            List<Lectura> lecturas = lecturasDAL.ObtenerLecturas();
+            this.grillaLecturas.DataSource = lecturas;
+
+            this.grillaLecturas.DataBind();
+
+
+        }
+
+        protected void btnFiltrarLectura_Click(object sender, EventArgs e)
+        {
+            int filtroNumSerie = Convert.ToInt32(this.FltLecturaDdl.SelectedItem.Text);
+
+
+            List<Lectura> medidores = lecturasDAL.ObtenerLecturas();
+            List<Lectura> medidoresFiltrados = medidores.FindAll(m => m.Medidor.NumSerie == filtroNumSerie);
+            this.grillaLecturas.DataSource = medidoresFiltrados;
+            this.grillaLecturas.DataBind();
+
+        }
+
+        public List<int> devuelveNSeries() {
+            List<int> listaNumSeries = new List<int>();
+            List<Lectura> lecturas = lecturasDAL.ObtenerLecturas();
+            
+            foreach (Lectura l in lecturas)
+            {
+                listaNumSeries.Add(l.Medidor.NumSerie);
+            }
+
+            return listaNumSeries = listaNumSeries.Distinct().ToList();
+        }
+
+
+        protected void btnRecargarLecturas_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("VerLecturas.aspx");
+
+        }
+    }
+}
