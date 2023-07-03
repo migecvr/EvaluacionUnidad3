a73da00de8a7b3dc9fff2f1f4d23651a07a9b134
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-02 22:24:21 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASP/IngresarMedidores.aspx.cs
@@ -0,0 +1,50 @@
+﻿using MedidorASPModel.DAL;
+using MedidorASPModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedidorASP
+{
+    public partial class IngresarMedidores : System.Web.UI.Page
+    {
+        private IMedidoresDAL medidoresDAL = new MedidoresDALObjetos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnAgregarMedidor_Click(object sender, EventArgs e)
+        {
+
+            //if (Page.IsValid)
+            //{
+            //    this.RequiredFieldValidator1.Text = "debes completar";
+
+            //}
+            //else {
+            //    this.RequiredFieldValidator1.Text = "Esta vacio";
+            //}
+
+            int numSerie = Convert.ToInt32(this.NumSerie.Text.Trim());
+
+
+            string tipo = this.TipoDdl.SelectedItem.Text;
+            //String tipo = this.Tipo.Text.Trim();
+
+            Medidor medidor = new Medidor()
+            {
+                NumSerie = numSerie,
+                Tipo = tipo
+
+            };
+            medidoresDAL.AgregarMedidor(medidor);
+
+            this.mensajeLbl.Text = "Se han ingresado los datos";
+
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
