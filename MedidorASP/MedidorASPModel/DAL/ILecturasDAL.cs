642e9385642eaeea820b1d9e4ad54da1c82fa4c6
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-02 22:21:45 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASPModel/DAL/ILecturasDAL.cs
@@ -0,0 +1,16 @@
+﻿using MedidorASPModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorASPModel.DAL
+{
+    public interface ILecturasDAL
+    {
+        List<Lectura> ObtenerLecturas();
+
+        void AgregarLectura(Lectura lectura);
+    }
+}
