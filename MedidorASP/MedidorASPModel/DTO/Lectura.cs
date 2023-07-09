da00c0e049e702c92fe1e66b8acb7573a8f67bcc
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-09 19:19:27 -0400

    Add files via upload

---
+++ MedidorASP/MedidorASPModel/DTO/Lectura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorASPModel.DTO
+{
+    public class Lectura
+    {
+        private Medidor medidor;
+        private String fecha;
+        private int hora;
+        private int minuto;
+        private double consumo;
+        
+
+        public Medidor Medidor { get => medidor; set => medidor = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public int Hora { get => hora; set => hora = value; }
+        public int Minuto { get => minuto; set => minuto = value; }
+        public double Consumo { get => consumo; set => consumo = value; }
+    }
+}
---
+++ MedidorASP/MedidorASPModel/DTO/Medidor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorASPModel.DTO
+{
+    public class Medidor
+    {
+        private int numSerie;
+        private string tipo;
+
+        public int NumSerie { get => numSerie; set => numSerie = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+
+
+    }
+}
